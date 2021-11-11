ebe708960e5a7286bf4bb3f29372eb6df30099d2
Johnny Chan <chanyao@umich.edu>
2021-11-11 16:40:10 +0800

    Finish comparison between leftist and min heap

--- prac/pq/main.c
+++ prac/pq/main.c
@@ -5,12 +5,10 @@
 #include <limits.h>  // INT_MAX
 
 const int SIZE_OF_N = 7;
-const int NUM_ELEMENTS[] = {100, 500, 1000, 2000, 3000, 4000, 5000}; // n
+const int MAX_RAND_SIZE = 10000;
+const int NUM_OF_ELEMENTS[] = {100, 500, 1000, 2000, 3000, 4000, 5000}; // n
 const int NUM_OF_OPERATIONS = 5000;                                  // m
-
-
-#define DELETE_MIN  0
-#define INSERT_MIN  1
+enum mode {LEFTIST_HEAP, MIN_HEAP};
 
 /************************************************************************
  *    This section contains code snippets for LeftistHeap only.
@@ -30,6 +28,16 @@ struct LeftistNode * New_LeftistNode1(int val)
 	node->left = NULL;
 	node->right = NULL;
 	return node;
+}
+
+void LeftistNode_free(LeftistNode *r) {
+	if (r == NULL) {
+		free(r);
+		return;
+	}
+	LeftistNode_free(r->left);
+	LeftistNode_free(r->right);
+	free(r);
 }
 
 struct LeftistNode * New_LeftistNode2(int _element, struct LeftistNode *_lt, struct LeftistNode *_rt, int _dist)
@@ -62,6 +70,7 @@ void LeftistHeap_deleteMin(LeftistHeap *);
 void LeftistHeap_insert(LeftistHeap *, int);
 bool LeftistHeap_isEmpty(LeftistHeap *);
 void LeftistHeap_removeAll(LeftistHeap *);
+void LeftistHeap_free(LeftistHeap *);
 
 int LeftistHeap_findMin(LeftistHeap *h) {
 	if (LeftistHeap_isEmpty(h)) return INT_MAX;
@@ -122,6 +131,11 @@ void LeftistHeap_removeAll(LeftistHeap *h) {
 	printf("\n-------------------------------\n");
 }
 
+void LeftistHeap_free(LeftistHeap *h) {
+	LeftistNode_free(h->root);
+	free(h);
+}
+
 /************************************************************************
  *    This section contains code snippets for MinHeap only.
  ************************************************************************/
@@ -141,6 +155,224 @@ struct MinHeap *New_MinHeap(int _capacity) {
 	return h;
 }
 
+void swap(int *x, int *y) {
+	int tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+/*** Forward declaration of member functions for LeftistHeap ***/
+void MinHeap_heapify(MinHeap *, int);
+int MinHeap_parent(int);
+int MinHeap_left(int);
+int MinHeap_right(int);
+int MinHeap_extractMin(MinHeap *);
+void MinHeap_decreaseKey(MinHeap *, int, int);
+int MinHeap_findMin(MinHeap *);
+void MinHeap_deleteKey(MinHeap *, int);
+void MinHeap_insertKey(MinHeap *, int);
+void MinHeap_removeAll(MinHeap *);
+void MinHeap_free(MinHeap *);
+
+void MinHeap_heapify(MinHeap *h, int i) {
+	int l = MinHeap_left(i);
+	int r = MinHeap_right(i);
+	int smallest = i;
+	if (l < h->size && *(h->arr + l) < *(h->arr + i))
+		smallest = l;
+	if (r < h->size && *(h->arr + r) < *(h->arr + smallest))
+		smallest = r;
+	if (smallest != i) {
+		swap(h->arr + i, h->arr + smallest);
+		MinHeap_heapify(h, smallest);
+	}
+}
+
+int MinHeap_parent(int i) {
+	return (i - 1) / 2;
+}
+
+int MinHeap_left(int i) {
+	return 2 * i + 1;
+}
+
+int MinHeap_right(int i) {
+	return 2 * i + 2;
+}
+
+int MinHeap_extractMin(MinHeap *h) {
+	int root;
+	if (h->size <= 0)
+		return INT_MAX;
+	else if (h->size == 1) {
+		h->size--;
+		return *(h->arr);
+	} else {
+		root = *(h->arr);
+		*(h->arr) = *(h->arr + h->size - 1);
+		h->size--;
+		MinHeap_heapify(h, 0);
+	}
+	return root;
+}
+
+void MinHeap_decreaseKey(MinHeap *h, int i, int val) {
+	*(h->arr + i) = val;
+	while (i && *(h->arr + MinHeap_parent(i)) > *(h->arr + i)) {
+		swap(h->arr + i, h->arr + MinHeap_parent(i));
+		i = MinHeap_parent(i);
+	}
+}
+
+int minHeap_findMin(MinHeap *h) {
+	return *(h->arr);
+}
+
+void MinHeap_deleteKey(MinHeap *h, int i) {
+	MinHeap_decreaseKey(h, i, INT_MIN);
+	MinHeap_extractMin(h);
+}
+
+void MinHeap_insertKey(MinHeap *h, int k) {
+	int i = h->size;
+	if (h->size == h->capacity) {
+		printf("Heap is full.\n");
+		return;
+	}
+	h->size++;
+	*(h->arr + i) = k;
+	while (i && *(h->arr + MinHeap_parent(i)) > *(h->arr + i)) {
+		swap(h->arr + i, h->arr + MinHeap_parent(i));
+		i = MinHeap_parent(i);
+	}
+}
+
+void MinHeap_removeAll(MinHeap *h) {
+	while (h->size) {
+		printf("%d ", MinHeap_extractMin(h));
+	}
+	printf("\n");
+}
+
+void MinHeap_free(MinHeap *h) {
+	free(h->arr);
+	free(h);
+}
+
+/************************************************************************
+ *    This section contains code snippets for utility functions.
+ ************************************************************************/
+int getNextAvailNum(bool *taken, int i) {
+	while (*(taken + i) == true) {
+		printf("i=%d ", i);
+		if (i+1 == MAX_RAND_SIZE) {
+			i = 0;
+		} else {
+			i++;
+		}
+	}
+	*(taken + i) = true;
+	return i;
+}
+
+void fill10KNumbers(int *arr) {
+	for(int i = 0; i < MAX_RAND_SIZE; ++i) {
+		*(arr + i) = i;
+	}
+	// use Fisher Yates shuffling algorithm
+	for (int j, i = MAX_RAND_SIZE - 1; i > 0; --i) {
+		j = rand() % (i + 1);
+		swap(arr + i, arr + j);
+	}
+}
+
+void fill5KOperations(bool *insert) {
+	for (int i = 0; i < NUM_OF_OPERATIONS; ++i) {
+		*(insert + i) = rand() % 2 == 0 ? true : false;
+	}
+}
+
+/************************************************************************
+ *    This section contains code snippets for the main program.
+ ************************************************************************/
+
+// create a LeftistHeap of n unique elements
+LeftistHeap *createLeftistHeap(int n, int *arr) {
+	LeftistHeap *h = New_LeftistHeap();
+	for (int i = n; i >= 0; --i) {
+		LeftistHeap_insert(h, *(arr+i));
+	}
+	return h;
+}
+
+MinHeap *createMinHeap(int n, int *arr) {
+	MinHeap *h = New_MinHeap(MAX_RAND_SIZE);
+	for (int i = n; i >= 0; --i) {
+		MinHeap_insertKey(h, *(arr+i));
+	}
+	return h;
+}
+
+void LeftistHeap_performOperations(LeftistHeap *h, int *arr, bool *insert) {
+	for (int i = 0; i < NUM_OF_OPERATIONS; ++i) {
+		if (*(insert + i)) {
+			LeftistHeap_insert(h, *(arr + NUM_OF_OPERATIONS + i));
+		} else {
+			LeftistHeap_deleteMin(h);
+		}
+	}
+}
+
+void MinHeap_performOperations(MinHeap *h, int *arr, bool *insert) {
+	for (int i = 0; i < NUM_OF_OPERATIONS; ++i) {
+		if (*(insert + i)) {
+			MinHeap_insertKey(h, *(arr+i));
+		} else {
+			MinHeap_extractMin(h);
+		}
+	}
+}
+
+
+void run(enum mode pq) {
+    clock_t clock_start, clock_end;;
+	bool *insert = malloc(NUM_OF_OPERATIONS * sizeof(bool));
+	int *arr = malloc(MAX_RAND_SIZE * sizeof(int));
+	fill10KNumbers(arr);
+	fill5KOperations(insert);
+
+	printf("-----------------------------------------------------\n");
+	for (int i = 0; i < SIZE_OF_N; ++i) {
+		int num = NUM_OF_ELEMENTS[i];
+
+		if (pq == LEFTIST_HEAP) {
+			LeftistHeap *h = createLeftistHeap(num, arr);
+			clock_start = clock();
+			LeftistHeap_performOperations(h, arr, insert);
+			clock_end = clock();
+			//LeftistHeap_removeAll(h);
+			LeftistHeap_free(h);
+			printf("Leftist heap starting with %d elements, average operation time %Lf/per operation\n",
+					num, (long double)(clock_end - clock_start) / CLOCKS_PER_SEC);
+		}
+
+		else if (pq == MIN_HEAP) {
+			MinHeap *t = createMinHeap(num, arr);
+			clock_start = clock();
+			MinHeap_performOperations(t, arr, insert);
+			clock_end = clock();
+			MinHeap_free(t);
+			printf("Min heap starting with %d elements, average operation time %Lf/per operation\n",
+					num, (long double)(clock_end - clock_start) / CLOCKS_PER_SEC);
+
+		}
+	} // for
+	printf("-----------------------------------------------------\n");
+
+	free(insert);
+	free(arr);
+}
+
 void test() {
     int i = 0;
     clock_t clock_start = clock();
@@ -148,7 +380,7 @@ void test() {
     int OPERATIONS[NUM_OF_OPERATIONS];
     for ( ;i < NUM_OF_OPERATIONS; ++i) {
         OPERATIONS[i] = rand() % 2;
-        printf("%d ", NUM_ELEMENTS[rand() % SIZE_OF_N]);
+        printf("%d ", NUM_OF_ELEMENTS[rand() % SIZE_OF_N]);
     }
     for ( i = 0; i < NUM_OF_OPERATIONS; ++i) {
         printf("%d", OPERATIONS[i]);
@@ -166,6 +398,7 @@ void test1() {
 		LeftistHeap_insert(h, arr[i]);
 	}
 	LeftistHeap_removeAll(h);
+	LeftistHeap_free(h);
 }
 
 void test2() {
@@ -177,11 +410,20 @@ void test2() {
 }
 
 void test3() {
+	int arr[] = {7,4,2,8,1,5,6,0,3,9};
 	MinHeap *h = New_MinHeap(10);
+	for (int i = 0; i < 10; ++i) {
+		MinHeap_insertKey(h, arr[i]);
+	}
+	MinHeap_removeAll(h);
+	MinHeap_free(h);
 }
 
 
 int main() {
-    test3();
+	enum mode leftist = LEFTIST_HEAP;
+	enum mode minheap = MIN_HEAP;
+	run(leftist);
+	run(minheap);
     return 0;
 }
